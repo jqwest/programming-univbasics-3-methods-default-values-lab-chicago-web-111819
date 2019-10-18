4163ab439fff49cd8ba7817bb9e858998e836df3
john L <johnleavell@gmail.com>
2019-10-18 01:43:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,5 +4,5 @@ def meal_choice(veg1, veg2, protien = 'meat')
   print "A plate of #{protien} with #{veg1} and #{veg2}."
 end
 
-meal_choice("broccoli", "macaroni", "tofu")
-meal_choice("broccoli", "macaroni")
+puts meal_choice("broccoli", "macaroni", "tofu")
+puts meal_choice("broccoli", "macaroni")
